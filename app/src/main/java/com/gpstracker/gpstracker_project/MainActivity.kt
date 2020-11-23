4cc07d94ae26fa6f10d48aad4204a4eb8d19204d
Jennifer Reitbauer <jennifer.reitbauer@evon-automation.com>
2020-11-23 19:10:25 +0100

    Initial commit

---
+++ app/src/main/java/com/gpstracker/gpstracker_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gpstracker.gpstracker_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
