76970883136a34c07d70c02c6ae834bc6dc8ac0c
thomas.hohenadler@gmail.com <thomas.hohenadler@gmail.com>
2020-11-23 23:33:29 +0100

    MainActivity.kt pseudologin CurrentActivity anzeigen

---
+++ app/src/main/java/com/gpstracker/gpstracker_project/CurrentActivity.kt
@@ -0,0 +1,18 @@
+package com.gpstracker.gpstracker_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CurrentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.current_activity)
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/gpstracker/gpstracker_project/MainActivity.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.gpstracker.gpstracker_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 //test
 //test 1 worked
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+
+    private val preferences = Preferences()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+        loginButton.setOnClickListener(this)
+
+
+        // Don't show MainActivity when user is already logged in
+        if (preferences.isUserLoggedIn(this)) {
+            // Go to CurrentActivity
+            val intent = Intent(this, CurrentActivity::class.java)
+            startActivity(intent)
+
+            // Finish MainActivity
+            finish()
+        }
+
+    }
+
+
+    override fun onClick(v: View?) {
+        // Display Toast
+        Toast.makeText(this, R.string.logged_in, Toast.LENGTH_LONG).show()
+
+        // Go to CurrentActivity
+        val intent = Intent(this, CurrentActivity::class.java)
+        startActivity(intent)
+
+        // Save login state
+        preferences.setUserLoggedIn(this, true)
+
+        // Finish MainActivity
+        finish()
     }
 }
