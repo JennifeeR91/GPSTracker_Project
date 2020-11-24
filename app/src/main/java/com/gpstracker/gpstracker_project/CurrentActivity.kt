e0ab5429bfdbfa1a866dbd8ca6b17bb4c9d4931e
thomas.hohenadler@gmail.com <thomas.hohenadler@gmail.com>
2020-11-24 23:35:27 +0100

    Menu update

--- app/src/main/java/com/gpstracker/gpstracker_project/CurrentActivity.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/CurrentActivity.kt
@@ -1,13 +1,39 @@
 package com.gpstracker.gpstracker_project
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CurrentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.current_activity)
+
+        Toast.makeText(this, R.string.test, Toast.LENGTH_LONG).show()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    Toast.makeText(this, R.string.logged_in, Toast.LENGTH_LONG).show()
+
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.page_2 -> {
+                    Toast.makeText(this, R.string.logged_in, Toast.LENGTH_LONG).show()
+
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 
