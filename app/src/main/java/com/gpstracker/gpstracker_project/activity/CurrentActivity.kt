e1f818f0b057e8db7fa107eda5d621cfe9d3c343
Jennifer Reitbauer <jennifer.reitbauer@evon-automation.com>
2020-12-01 21:37:04 +0100

    deleted fragment of build error

--- app/src/main/java/com/gpstracker/gpstracker_project/activity/CurrentActivity.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/activity/CurrentActivity.kt
@@ -4,19 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gpstracker.gpstracker_project.R
 import kotlinx.android.synthetic.main.current_activity.*
 
 
-class CurrentActivity : AppCompatActivity() {
+class CurrentActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.current_activity)
         tvPageTitle.text = "Activity"
 
-        // Bottom Naviagation
+        //get mapFragment
+        //val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+      //  mapFragment.getMapAsync(this)
+
+        // Bottom Navigation
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         //set current as active in navigation
         bottomNavigationView.getMenu().findItem(R.id.activity_page).setChecked(true)
@@ -61,6 +70,8 @@ class CurrentActivity : AppCompatActivity() {
 
     }
 
-
+    override fun onMapReady(googleMap: GoogleMap?) {
+        this.googleMap = googleMap
+    }
 
 }
