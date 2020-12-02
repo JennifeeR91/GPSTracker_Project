9b4392c753ba176997bf2fbdaa1e58035bd5f913
Jennifer Reitbauer <jennifer.reitbauer@evon-automation.com>
2020-12-02 20:02:14 +0100

    next step to show current position

--- app/src/main/java/com/gpstracker/gpstracker_project/activity/CurrentActivity.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/activity/CurrentActivity.kt
@@ -1,10 +1,15 @@
 package com.gpstracker.gpstracker_project.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -17,23 +22,26 @@ import kotlinx.android.synthetic.main.current_activity.*
 
 class CurrentActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private var googleMap: GoogleMap? = null
+   // private var googleMap: GoogleMap? = null
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val permissionCode = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.current_activity)
         tvPageTitle.text = "Activity"
 
-        //get mapFragment
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        //initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this!!)
+        fetchLocation()
 
         // Bottom Navigation
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         //set current as active in navigation
         bottomNavigationView.getMenu().findItem(R.id.activity_page).setChecked(true)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
+            when (item.itemId) {
                 R.id.activity_page -> {
                     // Go to CurrentActivity
                     val intent = Intent(this, CurrentActivity::class.java)
@@ -41,7 +49,7 @@ class CurrentActivity : AppCompatActivity(), OnMapReadyCallback {
                     startActivity(intent)
                     // Finish Activity
                     finish()
-                   true
+                    true
                 }
                 R.id.history_page -> {
                     // Go to HistoryActivity
@@ -73,14 +81,50 @@ class CurrentActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+        val task = fusedLocationClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                    currentLocation = location
+                    Toast.makeText(applicationContext, currentLocation.latitude.toString() + "" +
+                            currentLocation.longitude, Toast.LENGTH_SHORT).show()
+                    val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.fragment_map) as
+                            SupportMapFragment?)!!
+                    supportMapFragment.getMapAsync(this)
+                }
+            }
+        }
+
     override fun onMapReady(googleMap: GoogleMap?) {
-        var latitude = 0.0
-        var longitude = 0.0
+      // this.googleMap = googleMap
+       val latitudeStart = 10.3181466
+       val longitudeStart =  123.9029382
+     //  val latitudeEnd = 10.311795
+      // val longitudeEnd =  123.915864
 
-        this.googleMap = googleMap
+         googleMap!!.addMarker(MarkerOptions().position(LatLng(latitudeStart, longitudeStart)).title("Ayala"))
+    //    googleMap!!.addMarker(MarkerOptions().position(LatLng(latitudeEnd, longitudeEnd)).title("SM City"))
 
-        googleMap!!.addMarker(MarkerOptions().position(LatLng(latitude, longitude)).title("Current Location"))
+     //  googleMap!!.addMarker(MarkerOptions().position(LatLng(currentLocation.latitude, currentLocation.longitude)).title("Ayala"))
 
+
+      }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED) {
+            fetchLocation()
+        }
+        }
     }
-
 }
