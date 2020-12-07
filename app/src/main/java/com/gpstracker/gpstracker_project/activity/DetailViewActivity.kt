69332d180485a583e331b8310b228debd1b26c5c
thomas.hohenadler@gmail.com <thomas.hohenadler@gmail.com>
2020-12-07 21:18:26 +0100

    DetailViewActivity.kt shows details in textformat

--- app/src/main/java/com/gpstracker/gpstracker_project/activity/DetailViewActivity.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/activity/DetailViewActivity.kt
@@ -1,19 +1,81 @@
 package com.gpstracker.gpstracker_project.activity
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.gpstracker.gpstracker_project.Database
 import com.gpstracker.gpstracker_project.R
-import kotlinx.android.synthetic.main.result_activity.*
+import kotlinx.android.synthetic.main.detail_view_activity.*
+
+
+
 
 class DetailViewActivity : AppCompatActivity()  {
+
+    private val db = Database(this)
+    // new instance of resultactivity class to use getTime function
+    val result = ResultActivity()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_view_activity)
+
+        // get ID for Detailview
+        val id = intent.getLongExtra("id", -1)
+
         // page Title
         tvPageTitle.text = "Activity Detail"
-
+        tvPageTitle.append(" " + id)
         //val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.fragment_map1) as SupportMapFragment?)!!
         //supportMapFragment.getMapAsync(this)
+
+
+
+
+        if (id >= 0) {
+            showDataAsText(id)
+        }else{
+            Toast.makeText(applicationContext,  "Error: No ID given!!" , Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    private fun showDataAsText(id:Long) {
+        // get data from database
+        var activity = db.getActivity(id)
+
+        // output data as text
+        val tv_dynamic = TextView(this)
+        tv_dynamic.textSize = 20f
+
+
+        // ausgabe der Datenbankeinträge, sollte dann in eine eigene funktion
+
+            // get time
+        //val time = result.getDuration(i.starttime, i.endtime )
+
+
+
+            tv_dynamic.append(
+                    "id: " + activity?.id  +System.getProperty ("line.separator")+
+                    "startlong: " + activity?.startlong.toString()  +System.getProperty ("line.separator")+
+                    "endlong: " + activity?.endlong  +System.getProperty ("line.separator")+
+                    "startlat: " + activity?.startlat  +System.getProperty ("line.separator")+
+                    "endlat: " + activity?.endlat  +System.getProperty ("line.separator")+
+                    "starttime: " + activity?.starttime  +System.getProperty ("line.separator")+
+                    "endtime: " + activity?.endtime  +System.getProperty ("line.separator")+
+                    "note: " + activity?.note  +System.getProperty ("line.separator")
+
+            )
+
+
+
+
+        layout.addView(tv_dynamic)
+        return
 
     }
 
