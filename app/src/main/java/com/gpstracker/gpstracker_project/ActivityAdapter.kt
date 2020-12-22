5c2a7ce83aa792c78783462db7504eaf1cb29646
Jennifer Reitbauer <jennifer.reitbauer@evon-automation.com>
2020-12-22 08:16:05 +0100

    show types correctly in history

--- app/src/main/java/com/gpstracker/gpstracker_project/ActivityAdapter.kt
+++ app/src/main/java/com/gpstracker/gpstracker_project/ActivityAdapter.kt
@@ -8,15 +8,16 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import com.gpstracker.gpstracker_project.activity.ResultActivity
+import kotlinx.android.synthetic.main.result_activity.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
-class ActivityAdapter (val context: Context, var activities: List<Activity>): BaseAdapter() {
+class ActivityAdapter (context: Context, var activities: List<Activity>): BaseAdapter() {
 
     private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     val result = ResultActivity()
+    val gaittypes = context.resources.getStringArray(R.array.horse_gaits)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View
@@ -66,41 +67,24 @@ class ActivityAdapter (val context: Context, var activities: List<Activity>): Ba
         //set image & Title:
         val type = activity.activitytype
         println("type is hier"+ type)
-
-
-        // SET TITLE
-        //get items of array
-        val gaits  = context.getResources().getStringArray(R.array.horse_gaits)
-        val gait = activity.activitytype.toInt()
-        // Title
-        tvActivityTitle.text = gaits[gait]
-
-        // Image
-        //val drawableName = gaits[gait]
-        //imageView_icon.setImageResource(R.drawable.drawableName)
-
-
-
-        // SET IMAGE
         if(type == 0L) {
             imageView_icon.setImageResource(R.drawable.ic_logo_transparent)
-            //tvActivityTitle.text = activity.activitytype.toString() + " Walk"
-         //   setText(activity.activityType + arrayOf(R.array.horse_gaits))
+            tvActivityTitle.text = gaittypes[0]
         }
         else if(type == 1L) {
             imageView_icon.setImageResource(R.drawable.ic_logo_transparent)
-            //tvActivityTitle.text = activity.activitytype.toString() + " Trot"
+            tvActivityTitle.text = gaittypes[1]
         }
         else if(type == 2L){
             imageView_icon.setImageResource(R.drawable.ic_account)
-            //tvActivityTitle.text = activity.activitytype.toString() + " Canter"
+            tvActivityTitle.text = gaittypes[2]
         }else if(type ==3L) {
-            imageView_icon.setImageResource(R.drawable.ic_baseline_stats)
-            //tvActivityTitle.text = activity.activitytype.toString() + " Gallop"
+            imageView_icon.setImageResource(R.drawable.ic_newactivity)
+            tvActivityTitle.text = gaittypes[3]
         }
         else if(type ==4L) {
-            imageView_icon.setImageResource(R.drawable.ic_history)
-            //tvActivityTitle.text = activity.activitytype.toString() + " Pace"
+            imageView_icon.setImageResource(R.drawable.ic_newactivity)
+            tvActivityTitle.text = gaittypes[4]
         }
 
         // Set text on TextViews
